7b6db08ceb164c352e6019a119ef021f1f863fbf
alhamami <89762635+alhamami@users.noreply.github.com>
2021-09-09 16:57:45 +0300

    Update config.py

--- config.py
+++ config.py
@@ -9,6 +9,6 @@ DEBUG = True
 # Connect to the database
 
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = 'postgres://abskbousjroxwx:9b24f629e579f4dd9a33d26eadb6d7de70fa5237a80a63d673bca76fe732dac6@ec2-54-145-188-92.compute-1.amazonaws.com:5432/d3oaclh4elpp9b'
+SQLALCHEMY_DATABASE_URI = 'postgres://fmzbxccjemottn:87bde68550a3d51f5a7a215ecd607c44e8792a8e5ec842d7011a59a2a5408653@ec2-52-0-93-3.compute-1.amazonaws.com:5432/d3sc7ooguj4bjf'
 # Remove console warning
 SQLALCHEMY_TRACK_MODIFICATIONS = False
